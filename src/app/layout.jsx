39c8c34ee95f1df2df65b3a4adb86144bdcbadac
amirulabdlatib <amirulal73@gmail.com>
2025-12-31 11:14:29 +0800

    Project cleanup

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,13 @@ import "./globals.css";
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                <header>
+                    <nav>Nav</nav>
+                </header>
+                <main>{children}</main>
+                <footer>footer</footer>
+            </body>
         </html>
     );
 }
