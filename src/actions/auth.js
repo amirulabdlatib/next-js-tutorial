2fc288d298421d1818b381c3c165edc76627a932
amirulabdlatib <amirulal73@gmail.com>
2025-12-31 17:29:38 +0800

    08 Cookies and Session with JWT

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,6 +4,7 @@ import { RegisterFormSchema } from "@/lib/rules";
 import { getCollection } from "@/lib/db.js";
 import bcrypt from "bcrypt";
 import { redirect } from "next/navigation";
+import { createSession } from "@/lib/sessions";
 
 export async function register(state, formData) {
     const validatedFields = RegisterFormSchema.safeParse({
@@ -32,8 +33,10 @@ export async function register(state, formData) {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const results = await userCollection.insertOne({ email, password: hashedPassword });
+
+    // create sessions
+    await createSession(results.insertedId.toString());
 
     redirect("/dashboard");
 }
