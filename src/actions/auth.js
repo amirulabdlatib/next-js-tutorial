79eb7adf8fb508e4d8c36044481be32925a59fa4
amirulabdlatib <amirulal73@gmail.com>
2025-12-31 16:24:41 +0800

    07 Database integration with mongodb

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,9 @@
 "use server";
+
 import { RegisterFormSchema } from "@/lib/rules";
+import { getCollection } from "@/lib/db.js";
+import bcrypt from "bcrypt";
+import { redirect } from "next/navigation";
 
 export async function register(state, formData) {
     const validatedFields = RegisterFormSchema.safeParse({
@@ -14,4 +18,22 @@ export async function register(state, formData) {
             email: formData.get("email"),
         };
     }
+
+    const { email, password } = validatedFields.data;
+    const userCollection = await getCollection("users");
+
+    if (!userCollection) {
+        return { errors: { email: "Server error!" } };
+    }
+
+    const existingUser = await userCollection.findOne({ email });
+    if (existingUser) {
+        return { errors: { email: "Email already exists!" } };
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const results = await userCollection.insertOne({ email, password: hashedPassword });
+
+    redirect("/dashboard");
 }
